d830d29c5f8e698a38c411d0871e250d02bb64a5
DL123-a <dannylealgt@gmail.com>
2026-01-16 09:44:38 -0600

    arrgelo de ssalidas

--- RadioReal.java
+++ RadioReal.java
@@ -1,62 +1,69 @@
-import java.util.EmptyStackException;
-
 public class RadioReal implements Radio {
 
     double contAm = 530;
     double contFm = 87.9;
-    Boolean frecuencia = false;
+    boolean frecuencia = false; // false = FM, true = AM
     double[] estaciones = new double[12];
 
     public void prenderRadio(){
         System.out.println("Radio encendida");
     }
-	public void apagarRadio(){
+
+    public void apagarRadio(){
         System.out.println("Radio apagada");
     }
-	public void avanzarEstacion(){
-        if(frecuencia){
-            if(contAm==1610){
-                contAm=530;
-            }else{
-                contAm+=10;
-            }
-            System.out.println(contAm+" AM");
-        }else{
-            if(contFm==107.9){
-                contFm=81.9;
-            }else{
-                contFm+=0.2;
-            }
-            System.out.println(contFm+" FM");
-        }
-    }
-	public void guardarEstacion(int numeroBoton){
-        if(frecuencia){
-            estaciones[numeroBoton]=contAm;
-            System.out.println("Estación "+contAm+" AM guardada en botón "+numeroBoton);
-        }else{
-            estaciones[numeroBoton]=contFm;
-            System.out.println("Estación "+contFm+" FM guardada en botón "+numeroBoton);
 
+    public void avanzarEstacion(){
+        if (frecuencia) { // AM
+            if (contAm == 1610) {
+                contAm = 530;
+            } else {
+                contAm += 10;
+            }
+            System.out.println((int) contAm + " AM");
+        } else { // FM
+            if (contFm == 107.9) {
+                contFm = 87.9;
+            } else {
+                contFm += 0.2;
+            }
+            System.out.printf("%.1f FM%n", contFm);
         }
     }
-	public void cargarEstacion(int numeroBoton){
+
+    public void guardarEstacion(int numeroBoton){
+        if (frecuencia) {
+            estaciones[numeroBoton] = contAm;
+            System.out.println((int) contAm + " AM guardada en botón " + numeroBoton);
+        } else {
+            estaciones[numeroBoton] = contFm;
+            System.out.printf("%.1f FM guardada en botón %d%n", contFm, numeroBoton);
+        }
+    }
+
+    public void cargarEstacion(int numeroBoton){
         double estacion = estaciones[numeroBoton];
-        if(estacion%10==0){
-            frecuencia=true;
-            System.out.println(contAm+" AM");
-        }
-        else{
-            frecuencia=false;
-            System.out.println(contFm+" FM");
+
+        if (estacion % 10 == 0) { // AM
+            frecuencia = true;
+            contAm = estacion;
+            System.out.println((int) contAm + " AM");
+        } else { // FM
+            frecuencia = false;
+            contFm = estacion;
+            System.out.printf("%.1f FM%n", contFm);
         }
     }
-	public void cambiarFM(){
-        frecuencia=false;
-        System.out.println("Frecuencia Fm");
+
+    public void cambiarFM(){
+        frecuencia = false;
+        System.out.println("Frecuencia FM");
+        System.out.printf("%.1f FM%n", contFm);
     }
-	public void cambiarAM(){
-        frecuencia=true;
-        System.out.println("Frecuencia Fm");
+
+    public void cambiarAM(){
+        frecuencia = true;
+        System.out.println("Frecuencia AM");
+        System.out.println((int) contAm + " AM");
     }
 }
